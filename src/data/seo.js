12f3552b34df67c350046398d59d001bc26af70c
Khaerul Muhaemin <rkkhaerul@gmail.com>
2025-06-12 23:42:51 +0700

    FrontEnd ReactJS Batik Classification

---
+++ src/data/seo.js
@@ -0,0 +1,38 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"This project focuses on applying artificial intelligence, specifically classifying batik into 26 classes using convolutional neural networks (CNN).",
+		keywords: ["Deep Learning", "Batik Classification", "Research"],
+	},
+
+	{
+		page: "about",
+		description:
+			"This project focuses on applying artificial intelligence, specifically classifying batik into 26 classes using convolutional neural networks (CNN).",
+		keywords: ["Deep Learning", "Batik Classification", "Research"],
+	},
+
+	{
+		page: "articles",
+		description:
+			"This project focuses on applying artificial intelligence, specifically classifying batik into 26 classes using convolutional neural networks (CNN).",
+		keywords: ["Deep Learning", "Batik Classification", "Research"],
+	},
+
+	{
+		page: "projects",
+		description:
+			"This project focuses on applying artificial intelligence, specifically classifying batik into 26 classes using convolutional neural networks (CNN).",
+		keywords: ["Deep Learning", "Batik Classification", "Research"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"This project focuses on applying artificial intelligence, specifically classifying batik into 26 classes using convolutional neural networks (CNN).",
+		keywords: ["Deep Learning", "Batik Classification", "Research"],
+	},
+];
+
+export default SEO;
---
+++ src/data/user.js
@@ -0,0 +1,86 @@
+const INFO = {
+	main: {
+		title: "Batik Classification",
+		name: "Computing Project Team 3",
+		email: "@student.telkomuniversity.ac.id",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://www.linkedin.com/in/",
+		instagram: "https://www.instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://www.facebook.com/",
+	},
+
+	homepage: { 
+		title: "Batik Classification",
+		description:
+			"The Batik classification system we developed can identify 26 categories, Using an AI-powered approach, this model helps detect and classify motifs with high accuracy, thus supporting Indonesia's cultural knowledge",
+	},	
+
+	about: {
+		title: "I’m the developer behind an intelligent Batik Classification system.",
+		description:
+			"This project focuses on classifying Batik into 26 main categories",
+	},
+	
+	articles: {
+		title: "Exploring the intersection of deep learning",
+		description:
+			"A chronological collection of technical write-ups, experiments, and insights into building a Batik Classification model. Topics range from CNN architectures, model optimization, dataset challenges, to real-world applications.",
+	},
+	
+	projects: [
+		{
+			title: "Batik Barong",
+			description:
+				"Batik Barong Bali – Menampilkan figur mitologis Barong, simbol pelindung spiritual yang merefleksikan dualitas karakter manusia dan kekuatan maskulin.",
+			logo: "https://i.postimg.cc/KYQJ1pMw/Barong-0026.jpg",
+			link: "#",
+		},
+	
+		{
+			title: "Batik Ondel-Ondel",
+			description:
+				"Batik Onde-ondel Jakarta – Motif ambalan Betawi yang menampilkan boneka ondel-ondel, mewakili keselamatan dan semangat budaya Jakarta.",
+			logo: "https://i.postimg.cc/g2TJbvjb/Ondel-Ondel-0022.jpg",
+			link: "#",
+		},
+	
+		{
+			title: "Batik Megamendung",
+			description:
+				"Batik Megamendung Jawa Barat – Pola awan megamendung mencerminkan keteduhan, kesabaran, dan kepemimpinan yang rendah hati.",
+			logo: "https://i.postimg.cc/4N3s4mrf/Megamendung-0157.jpg",
+			link: "#",
+		},
+	
+		{
+			title: "Batik Parang",
+			description:
+				"Batik Parang Jawa Tengah – Garis diagonal ‘S’ yang saling berkait melambangkan tekad, keberanian, dan perjuangan tanpa henti.",
+			logo: "https://i.postimg.cc/brgnxsTY/Parang-Rusak-0012.jpg",
+			link: "#",
+		},
+		{
+			title: "Batik Asmat",
+			description:
+				"Batik Asmat Papua – Dipenuhi motif khas suku Asmat, menggambarkan hubungan dengan alam dan warisan budaya leluhur Papua.",
+			logo: "https://i.postimg.cc/QdGDQ0Ft/Asmat-0196.jpg",
+			link: "#",
+		},
+		{
+			title: "Batik Kawung",
+			description:
+				"Batik Kawung Yogyakarta – Pola geometris oval menyerupai kolang-kaling yang melambangkan kesucian, pengendalian diri, dan akar budaya keraton Yogyakarta.",
+			logo: "https://i.postimg.cc/ZK3nz93W/Kawung-0056.jpg",
+			link: "#",
+		},
+	]
+	
+};
+
+export default INFO;
